77b366970daf6cff938b725d76008c70e114e874
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-03-28 16:53:45 -0300

    First commit

---
+++ src/main/java/br/com/rhprojecth2/RhprojectH2Application.java
@@ -0,0 +1,13 @@
+package br.com.rhprojecth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhprojectH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhprojectH2Application.class, args);
+	}
+
+}
