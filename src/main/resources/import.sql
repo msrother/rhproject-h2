7a8405347bef1e6d40c302c6702cfb8c122f5220
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-03-28 19:25:25 -0300

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_departments(dpt_name) VALUES ('Marketing');
+INSERT INTO tb_departments(dpt_name) VALUES ('Contábil');
+INSERT INTO tb_departments(dpt_name) VALUES ('Financeiro');
+INSERT INTO tb_departments(dpt_name) VALUES ('TI');
+
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Maria Silva', 'Rua das Margaridas 234', '2021-02-23', 2);
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Bob Amarante', 'Av. Botinal 555', '2010-01-13', 3);
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Alex Titius', 'Rua Laranepes 432', '2020-05-27', 1);
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Laura Pitt', 'Rua Pantanal 211', '2011-11-30', 2);
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Mario Bordi', 'Av. Castelão 897', '2019-06-29', 4);
+INSERT INTO tb_employees(emp_name, emp_address, emp_hiringDate, dpt_id) VALUES ('Kalin Linnas', 'Rua da Distron 2311', '2017-08-23', 4);
+
