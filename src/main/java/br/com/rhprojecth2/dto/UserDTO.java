aa62ded92224ea3829e1d5a5f8e6f3d12d9b756c
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-01 11:18:57 -0300

    Create UserDTO

---
+++ src/main/java/br/com/rhprojecth2/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package br.com.rhprojecth2.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_users")
+public class UserDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usr_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "usr_password", nullable = false)
+    private String password;
+
+}
