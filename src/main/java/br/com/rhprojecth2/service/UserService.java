84466de4226b1aed2132792a2f3dd0eab2d75244
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-02 16:55:56 -0300

    Create UserService

---
+++ src/main/java/br/com/rhprojecth2/service/UserService.java
@@ -0,0 +1,39 @@
+package br.com.rhprojecth2.service;
+
+import br.com.rhprojecth2.dto.UserDTO;
+import br.com.rhprojecth2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDTO save(UserDTO dto) {
+        return userRepository.save(dto);
+    }
+
+    public List<UserDTO> listAll(UserDTO dto) {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserDTO> searchById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public UserDTO searchByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<UserDTO> searchByPassword(String password) {
+        return userRepository.findByPassword(password);
+    }
+
+    public void removeById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
