91e22afb58bd35b3eee8be1dd6fda7e57ee9c35e
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-03 16:47:41 -0300

    Update UserRepository

--- src/main/java/br/com/rhprojecth2/repository/UserRepository.java
+++ src/main/java/br/com/rhprojecth2/repository/UserRepository.java
@@ -2,9 +2,16 @@ package br.com.rhprojecth2.repository;
 
 import br.com.rhprojecth2.dto.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserDTO, Long> {
 
     public UserDTO findByEmail(String email);
+
+    @Query("SELECT u FROM UserDTO u WHERE u.password =:password ORDER BY u.id DESC")
+    public List<UserDTO> findByPassword(@Param("password") String password);
 
 }
