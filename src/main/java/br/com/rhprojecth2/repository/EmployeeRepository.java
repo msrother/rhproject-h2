7c39ff539cdbfd4b08749d277dd100d56be541db
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-04 22:39:22 -0300

    Update EmployeeRepository

--- src/main/java/br/com/rhprojecth2/repository/EmployeeRepository.java
+++ src/main/java/br/com/rhprojecth2/repository/EmployeeRepository.java
@@ -2,11 +2,22 @@ package br.com.rhprojecth2.repository;
 
 import br.com.rhprojecth2.dto.EmployeeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeDTO, Long> {
 
     public List<EmployeeDTO> findByNameContaining (String name);
+
+    @Query(nativeQuery = true, value =
+            "SELECT e.* FROM tb_employees e INNER JOIN tb_departments d ON d.id = e.dpt_id "
+            + "WHERE e.dpt_id = :dptId ORDER BY e.emp_name ASC")
+    public List<EmployeeDTO> findByDepartmentId(@Param("dptId") Long dptId);
+
+
+
+
 
 }
