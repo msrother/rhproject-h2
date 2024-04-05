ccb30e792f12c400e0bd3a850262207617e69c92
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-04 22:41:24 -0300

    Create EmployeeController

---
+++ src/main/java/br/com/rhprojecth2/controller/EmployeeController.java
@@ -0,0 +1,75 @@
+package br.com.rhprojecth2.controller;
+
+import br.com.rhprojecth2.dto.EmployeeDTO;
+import br.com.rhprojecth2.service.EmployeeService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/rhprojecth2/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping(value = "/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee) {
+        return employeeService.save(employee);
+    }
+
+    @GetMapping(value = "/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EmployeeDTO> listAll() {
+        return employeeService.listAll();
+    }
+
+    @GetMapping(value = "/searchById")
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDTO searchById(@RequestHeader(value = "id") Long id) {
+        return employeeService.searchById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee not found."));
+    }
+
+    @GetMapping(value = "/searchByName")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EmployeeDTO> searchByName(@RequestHeader(value = "name") String name) {
+        return employeeService.searchByName(name);
+    }
+
+    @GetMapping(value = "/searchByDepartmentId")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EmployeeDTO> searchByDepartmentId(@RequestHeader(value = "departmentId") Long departmentId) {
+        return employeeService.searchByDepartmentId(departmentId);
+    }
+
+    @PutMapping(value = "/update")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateEmployee(@RequestBody EmployeeDTO employee) {
+        employeeService.searchById(employee.getId())
+                .map(foundEmployee -> {
+                    modelMapper.map(employee, foundEmployee);
+                    employeeService.save(foundEmployee);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee not found."));
+    }
+
+    @DeleteMapping(value = "/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeEmployee(@RequestHeader(value = "id") Long id) {
+        employeeService.searchById(id)
+                .map(employeeFound -> {
+                    employeeService.removeById(employeeFound.getId());
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee not found."));
+    }
+
+}
