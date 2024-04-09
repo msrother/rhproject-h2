c7f383275733dc3dbc32e89a4b2149ef795ab24b
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-09 15:59:46 -0300

    Update DepartmentRepository

--- src/main/java/br/com/rhprojecth2/repository/DepartmentRepository.java
+++ src/main/java/br/com/rhprojecth2/repository/DepartmentRepository.java
@@ -3,6 +3,10 @@ package br.com.rhprojecth2.repository;
 import br.com.rhprojecth2.dto.DepartmentDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<DepartmentDTO, Long> {
+
+    public List<DepartmentDTO> findByNameContaining (String name);
 
 }
