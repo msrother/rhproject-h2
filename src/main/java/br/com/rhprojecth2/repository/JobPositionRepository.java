59df03a917399745122293749fd672661fc157c5
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-04-11 16:16:49 -0300

    Update JobPositionRepository

--- src/main/java/br/com/rhprojecth2/repository/JobPositionRepository.java
+++ src/main/java/br/com/rhprojecth2/repository/JobPositionRepository.java
@@ -3,6 +3,10 @@ package br.com.rhprojecth2.repository;
 import br.com.rhprojecth2.dto.JobPositionDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JobPositionRepository extends JpaRepository<JobPositionDTO, Long> {
+
+    public List<JobPositionDTO> findByNameContaining (String name);
 
 }
