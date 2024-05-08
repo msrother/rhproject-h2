8066fcd997a5ab1bb174b6b935465e7a0f0c2e9a
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-05-08 09:49:34 -0300

    Update DepartmentController with token validation

--- src/main/java/br/com/rhprojecth2/controller/DepartmentController.java
+++ src/main/java/br/com/rhprojecth2/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package br.com.rhprojecth2.controller;
 
 import br.com.rhprojecth2.dto.DepartmentDTO;
 import br.com.rhprojecth2.service.DepartmentService;
+import br.com.rhprojecth2.utils.TokenJWT;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,32 +24,41 @@ public class DepartmentController {
 
     @PostMapping(value = "/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public DepartmentDTO createDepartment(@RequestBody DepartmentDTO department) {
+    public DepartmentDTO createDepartment(@RequestBody DepartmentDTO department,
+                                          @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return departmentService.save(department);
     }
 
     @GetMapping(value = "/list")
     @ResponseStatus(HttpStatus.OK)
-    public List<DepartmentDTO> listAll() {
+    public List<DepartmentDTO> listAll(@RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return departmentService.listAll();
     }
 
     @GetMapping(value = "/searchById")
     @ResponseStatus(HttpStatus.OK)
-    public DepartmentDTO searchById(@RequestHeader(value = "id") Long id) {
+    public DepartmentDTO searchById(@RequestHeader(value = "id") Long id,
+                                    @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return departmentService.searchById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Department not found."));
     }
 
     @GetMapping(value = "/searchByName")
     @ResponseStatus(HttpStatus.OK)
-    public List<DepartmentDTO> searchByName(@RequestHeader(value = "name") String name) {
+    public List<DepartmentDTO> searchByName(@RequestHeader(value = "name") String name,
+                                            @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return departmentService.searchByName(name);
     }
 
     @PutMapping(value = "/update")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateDepartment(@RequestBody DepartmentDTO department) {
+    public void updateDepartment(@RequestBody DepartmentDTO department,
+                                 @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         departmentService.searchById(department.getId())
                 .map(foundDepartment -> {
                     modelMapper.map(department, foundDepartment);
@@ -59,7 +69,9 @@ public class DepartmentController {
 
     @DeleteMapping(value = "/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeDepartment(@RequestHeader(value = "id") Long id) {
+    public void removeDepartment(@RequestHeader(value = "id") Long id,
+                                 @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         departmentService.searchById(id)
                 .map(departmentFound -> {
                     departmentService.removeById(departmentFound.getId());
