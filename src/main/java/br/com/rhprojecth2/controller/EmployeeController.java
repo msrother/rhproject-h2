010a5245c95d9e60b9cadcfcaf587c922eab6f04
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2024-05-08 09:38:21 -0300

    Update EmployeeController with token validation

--- src/main/java/br/com/rhprojecth2/controller/EmployeeController.java
+++ src/main/java/br/com/rhprojecth2/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package br.com.rhprojecth2.controller;
 
 import br.com.rhprojecth2.dto.EmployeeDTO;
 import br.com.rhprojecth2.service.EmployeeService;
+import br.com.rhprojecth2.utils.TokenJWT;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,38 +23,49 @@ public class EmployeeController {
 
     @PostMapping(value = "/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee) {
+    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employee,
+                                      @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return employeeService.save(employee);
     }
 
     @GetMapping(value = "/list")
     @ResponseStatus(HttpStatus.OK)
-    public List<EmployeeDTO> listAll() {
+    public List<EmployeeDTO> listAll(@RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return employeeService.listAll();
     }
 
     @GetMapping(value = "/searchById")
     @ResponseStatus(HttpStatus.OK)
-    public EmployeeDTO searchById(@RequestHeader(value = "id") Long id) {
+    public EmployeeDTO searchById(@RequestHeader(value = "id") Long id,
+                                  @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return employeeService.searchById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee not found."));
     }
 
     @GetMapping(value = "/searchByName")
     @ResponseStatus(HttpStatus.OK)
-    public List<EmployeeDTO> searchByName(@RequestHeader(value = "name") String name) {
+    public List<EmployeeDTO> searchByName(@RequestHeader(value = "name") String name,
+                                          @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return employeeService.searchByName(name);
     }
 
     @GetMapping(value = "/searchByDepartmentId")
     @ResponseStatus(HttpStatus.OK)
-    public List<EmployeeDTO> searchByDepartmentId(@RequestHeader(value = "departmentId") Long departmentId) {
+    public List<EmployeeDTO> searchByDepartmentId(@RequestHeader(value = "departmentId") Long departmentId,
+                                                  @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         return employeeService.searchByDepartmentId(departmentId);
     }
 
     @PutMapping(value = "/update")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateEmployee(@RequestBody EmployeeDTO employee) {
+    public void updateEmployee(@RequestBody EmployeeDTO employee,
+                               @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         employeeService.searchById(employee.getId())
                 .map(foundEmployee -> {
                     modelMapper.map(employee, foundEmployee);
@@ -64,7 +76,9 @@ public class EmployeeController {
 
     @DeleteMapping(value = "/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeEmployee(@RequestHeader(value = "id") Long id) {
+    public void removeEmployee(@RequestHeader(value = "id") Long id,
+                               @RequestHeader(value = "token") String token) {
+        TokenJWT.validateToken(token);
         employeeService.searchById(id)
                 .map(employeeFound -> {
                     employeeService.removeById(employeeFound.getId());
